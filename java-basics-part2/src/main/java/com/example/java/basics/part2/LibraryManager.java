e971d88dc63f0782ba4b43296ec331a7347c984d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-07 15:56:35 +0400

    initializeMembers() method Done

---
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/LibraryManager.java
@@ -0,0 +1,13 @@
+package com.example.java.basics.part2;
+
+import java.util.ArrayList;
+
+public class LibraryManager {
+    private ArrayList<LibraryMember> members = new ArrayList<>();
+
+    public void initializeMembers(){
+      members.add(new LibraryMember(101, "Ahmed", "Standard"));
+      members.add(new LibraryMember (102, "Sara", "Premium"));
+      members.add(new LibraryMember (103, "John", "Standard"));
+    }
+}
