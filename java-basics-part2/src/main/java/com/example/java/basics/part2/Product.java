5f4ffc998326f0824ac28d75ba3db3f3e32d09da
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-07 16:22:40 +0400

    class product is created

---
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/Product.java
@@ -0,0 +1,16 @@
+package com.example.java.basics.part2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+   private int productId;
+   private String productName;
+   private int price;
+
+
+}
