dcee2affa3890567f828d0e86994364de73a6f9f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-07 15:45:41 +0400

    task 34 Done

--- java-basics-part2/src/main/java/com/example/java/basics/part2/DeleteTaskDemo.java
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/DeleteTaskDemo.java
@@ -14,6 +14,24 @@ public class DeleteTaskDemo {
         for (String task : tasks) {
             System.out.println(task);
         }
+        String targetTask =  "Call doctor";
+
+        for (int i =0; i<tasks.size(); i++){
+            String firstTask= tasks.get(i);
+            if (firstTask.equalsIgnoreCase(targetTask)){
+                tasks.remove(targetTask);
+                System.out.println("Task Deleted successfully");
+                break;
+            }else {
+                System.out.println("Task not found, no deletion performed");
+
+            }
+        }
+
+        System.out.println("Final list: ");
+        for (String task : tasks) {
+            System.out.println(task);
+        }
     }
 
 
