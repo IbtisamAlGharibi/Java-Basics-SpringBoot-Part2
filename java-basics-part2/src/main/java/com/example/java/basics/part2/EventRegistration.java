ebcfab0a2f201d58c92d4aebaa7a2ec31b2a371e
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-08 08:50:25 +0400

    EventRegistration class is created

---
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/EventRegistration.java
@@ -0,0 +1,14 @@
+package com.example.java.basics.part2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class EventRegistration {
+    private int registrationId;
+    private String attendeeName;
+
+}
