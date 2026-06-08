de8cc9aa34488f6ef4fa8b0fc4c06b75e8367a74
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-08 08:51:50 +0400

    RegistrationService is cretaed with a map inside

---
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/RegistrationService.java
@@ -0,0 +1,9 @@
+package com.example.java.basics.part2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegistrationService {
+    Map<Integer, EventRegistration> registrations = new HashMap<>();
+
+}
