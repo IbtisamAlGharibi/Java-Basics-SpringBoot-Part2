62b6d1d7ff38d66dc96d51f11eada5e286f44457
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-08 08:57:38 +0400

    RegistrationService is cretaed with a map inside

--- java-basics-part2/src/main/java/com/example/java/basics/part2/RegistrationService.java
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/RegistrationService.java
@@ -1,9 +1,17 @@
 package com.example.java.basics.part2;
 
+import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
 import java.util.Map;
-
+@Service
 public class RegistrationService {
     Map<Integer, EventRegistration> registrations = new HashMap<>();
 
+    public RegistrationService() {
+        registrations.put(101, new EventRegistration(101, "Ahmed"));
+        registrations.put(102, new EventRegistration(102, "Sara"));
+        registrations.put(103, new EventRegistration(103, "John"));
+        registrations.put(104, new EventRegistration(104, "Fatma"));
+    }
 }
