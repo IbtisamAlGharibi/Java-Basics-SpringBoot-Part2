00cc6b77ab6271f0a2801aa3b4bad024c42cade1
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-08 10:56:15 +0400

    addVehicle() Done

--- java-basics-part2/src/main/java/com/example/java/basics/part2/Services/VehicleManager.java
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/Services/VehicleManager.java
@@ -1,5 +1,6 @@
 package com.example.java.basics.part2.Services;
 
+import com.example.java.basics.part2.Entity.Employee;
 import com.example.java.basics.part2.Entity.Vehicle;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,18 @@ public class VehicleManager {
         vehicles.add(new Vehicle("V101", "Toyota Corolla", 20));
         vehicles.add(new Vehicle("V102", "Nissan Sunny", 18));
         vehicles.add(new Vehicle("V103", "Hyundai Elantra", 22));
+    }
+
+    public String addVehicle(Vehicle vehicle){
+        System.out.println("List of vehicles: ");
+        for (Vehicle vehicleList : vehicles) {
+            System.out.println(vehicleList);
+
+            if (vehicleList.getVehicleId().equalsIgnoreCase(vehicle.getVehicleId())) {
+                return "vehicle already exists";
+            }
+        }
+       vehicles.add(vehicle);
+        return "Vehicle Added successfully";
     }
 }
