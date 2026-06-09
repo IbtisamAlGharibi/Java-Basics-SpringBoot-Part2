4fb024882d7464135655448baf31b296842edfc8
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-09 09:47:53 +0400

    Adding the annotations in the Employee class

--- java-basics-part2/src/main/java/com/example/java/basics/part2/Entity/Employee.java
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/Entity/Employee.java
@@ -1,5 +1,6 @@
 package com.example.java.basics.part2.Entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,8 +8,13 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private  String employeeId;
+
     private String employeeName;
     private String department;
 
