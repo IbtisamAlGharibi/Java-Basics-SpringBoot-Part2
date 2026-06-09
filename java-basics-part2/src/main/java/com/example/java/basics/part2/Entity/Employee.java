b993a0a12772c66e61fc6ab6c8b2473328998187
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-09 10:42:24 +0400

    Adding relationships and fixing some error

--- java-basics-part2/src/main/java/com/example/java/basics/part2/Entity/Employee.java
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/Entity/Employee.java
@@ -5,17 +5,32 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Getter
 @Setter
 @Entity
 @Table(name =" Employee")
 public class Employee {
+    public Employee(String employeeId,
+                    String employeeName,
+                    String department) {
+
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.department = department;
+    }
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  String employeeId;
 
     private String employeeName;
     private String department;
+    @OneToMany
+    List<Campaign> campaigns;
+    @OneToMany
+    List<Vehicle> vehicles;
 
 }
