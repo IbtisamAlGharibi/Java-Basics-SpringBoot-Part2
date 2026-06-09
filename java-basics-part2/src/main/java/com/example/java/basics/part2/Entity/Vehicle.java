c4d23689e75117578b9caacbc8804a0ce8f50f01
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-09 09:49:08 +0400

    Adding the annotations in the Vehicle class

--- java-basics-part2/src/main/java/com/example/java/basics/part2/Entity/Vehicle.java
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/Entity/Vehicle.java
@@ -1,5 +1,6 @@
 package com.example.java.basics.part2.Entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,8 +8,13 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String vehicleId;
+
     private String vehicleModel;
     private int rentalPricePerDay;
 
