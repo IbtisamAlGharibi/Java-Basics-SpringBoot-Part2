772061617c64a7029c0361b93d15662ce19b5a50
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-10 15:48:09 +0400

    GetMapping(getAll) Done

--- java-basics-part2/src/main/java/com/example/java/basics/part2/Controllers/VehicleController.java
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/Controllers/VehicleController.java
@@ -3,9 +3,12 @@ package com.example.java.basics.part2.Controllers;
 import com.example.java.basics.part2.Entity.Vehicle;
 import com.example.java.basics.part2.Services.VehicleManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class VehicleController {
@@ -16,5 +19,9 @@ public class VehicleController {
     @PostMapping("/add")
     public Vehicle addVehicle(@RequestBody Vehicle vehicle) {
         return vehicleManager.addVehicle(vehicle);
+    }
+    @GetMapping("getAll")
+    public List<Vehicle> getAll() {
+        return vehicleManager.getAllVehicles();
     }
 }
