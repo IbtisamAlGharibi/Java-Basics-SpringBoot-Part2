acbd1c4886f40a3b1a56388e0b7c4c9511f356b7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-10 16:15:04 +0400

    @GetMapping(getAll) Done

--- java-basics-part2/src/main/java/com/example/java/basics/part2/Controllers/CampaignController.java
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/Controllers/CampaignController.java
@@ -1,11 +1,15 @@
 package com.example.java.basics.part2.Controllers;
 
 import com.example.java.basics.part2.Entity.Campaign;
+import com.example.java.basics.part2.Entity.Vehicle;
 import com.example.java.basics.part2.Services.CampaignManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CampaignController {
@@ -16,5 +20,9 @@ public class CampaignController {
     public Campaign addCampaign(@RequestBody Campaign campaign) {
         return campaignManager.addCampaign(campaign);
     }
+    @GetMapping("getAll")
+    public List<Campaign> getAll() {
+        return campaignManager.getAllCampaigns();
 
+    }
 }
