9ebb85c4c1790ea24fc256c7656d9a758ea62e71
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-10 16:01:11 +0400

    Addition in CampaignRepository

--- java-basics-part2/src/main/java/com/example/java/basics/part2/Inferfaces/CampaignRepository.java
+++ java-basics-part2/src/main/java/com/example/java/basics/part2/Inferfaces/CampaignRepository.java
@@ -2,8 +2,20 @@ package com.example.java.basics.part2.Inferfaces;
 
 import com.example.java.basics.part2.Entity.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CampaignRepository extends JpaRepository<Campaign,String > {
+    @Query("SELECT c FROM Campaign WHERE c.isActive=true")
+    List<Campaign> getAllCampaign();
+
+    @Query("SELECT c from Campaign c WHERE c.isActive=true AND c.id=:campaignId")
+    Campaign getCampaignById(@Param("campaignId") Integer id);
+
+    @Query(" SELECT c from Campaign c WHERE c.isActive=true AND c.campaignName=:campaignName")
+    Campaign getCampaignByName(@Param("campaignName") String campaignName);
 }
